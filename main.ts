d1fa5e7e35fe2bfc5d4cc13eeaf1d59db7b67a76
ilarioni <ilia.meshvildishvili@gmail.com>
2021-03-31 15:23:41 +0200

    variables

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
 const a = "1";
+// transpile ts file into js
+
+//creating variables
+const hello = "world";
+
+// hello = "foo" - Cannot assign to 'hello' because it is a constant.ts(2588)
+// when creating variables with let, we can re-assign with the same type
+
+let hey = "sun";
+let hey = true;
+//Cannot redeclare block-scoped variable 'hey'.ts(2451)
+
+// when can explicitly declare variable
+let holla: string = "earth";
+holla = 2;  //Type 'number' is not assignable to type 'string'.ts(2322)
+
